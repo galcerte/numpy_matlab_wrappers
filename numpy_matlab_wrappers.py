5911b08c233193d6024da931f63a5a3ea1c88122
Agustín Galante <81190342+galcerte@users.noreply.github.com>
2022-07-17 13:49:51 +0200

    Added further flow control structures such that the shape of the input array is preserved

--- numpy_matlab_wrappers.py
+++ numpy_matlab_wrappers.py
@@ -2,9 +2,9 @@ import numpy as np
 
 def any(array: np.ndarray):
     '''
-    Partial implementation of the any() function from MATLAB.
-    It is partial in the sense that additional parameters aside from the
-    target array have not been considered.
+    Partial implementation of the any() function from MATLAB. It is partial in
+    the sense that additional parameters aside from the target array have not
+    been considered.
 
     More information about this function can be found in
     https://mathworks.com/help/matlab/ref/any.html
@@ -23,12 +23,22 @@ def any(array: np.ndarray):
 
     # We check that the parameter is of the right type
     if not isinstance(array, np.ndarray):
-        raise TypeError('array variable is not of type ndarray.')
+        raise TypeError('array parameter is not of type ndarray.')
 
-    # If it's a vector, we merely return True if any of its elements are True
-    if (len(array.shape) == 1) or ((len(array.shape) == 2) and ((array.shape[0] == 1) or (array.shape[1] == 1))):
+    # If we have a one-dimensional array, we merely return True if any of its
+    # elements are True
+    if len(array.shape) == 1:
         any = np.any(array)
-    # If it is a 2-dimensional array, we check for True values over columns
+    # If we have a two-dimensional array which has a size of 1 in either axis,
+    # we consider that as an equivalent of a MATLAB vector so we reshape the
+    # resulting array such that we preserve the shape of the original.
+    # This is because NumPy functions get rid of the axis that is equal to 1 in
+    # the output
+    elif len(array.shape) == 2 and (1 in array.shape):
+        any = np.any(array)
+        any = any.reshape(array.shape)
+    # If it is a two-dimensional array with no axis with size 1, we check for
+    # True values over columns
     elif (len(array.shape) == 2) and ((array.shape[0] != 1) and (array.shape[1] != 1)):
         any = np.any(array, axis=1)
     # If it is an n-dimensional array with n > 2, we check for True
@@ -43,9 +53,9 @@ def any(array: np.ndarray):
 
 def cumsum(array: np.ndarray):
     '''
-    Partial implementation of the behaviour of the cumsum() function from MATLAB.
-    It is partial in the sense that additional parameters aside from the
-    target array have not been considered.
+    Partial implementation of the behaviour of the cumsum() function from
+    MATLAB. It is partial in the sense that additional parameters aside from
+    the target array have not been considered.
 
     More information about this function can be found in
     https://mathworks.com/help/matlab/ref/cumsum.html
@@ -63,12 +73,22 @@ def cumsum(array: np.ndarray):
 
     # We check that the parameter is of the right type
     if not isinstance(array, np.ndarray):
-        raise TypeError('array variable is not of type ndarray.')
+        raise TypeError('array parameter is not of type ndarray.')
     
-    # If it's a vector, we merely calculate the cumulative sum over all of its elements
-    if (len(array.shape) == 1) or ((len(array.shape) == 2) and ((array.shape[0] == 1) or (array.shape[1] == 1))):
+    # If we have a one-dimensional array, we merely return True if any of its
+    # elements are True
+    if len(array.shape) == 1:
         cumsum = np.cumsum(array)
-    # If it's an array with two axis, we do the cumulative sum over its columns
+    # If we have a two-dimensional array which has a size of 1 in either axis,
+    # we consider that as an equivalent of a MATLAB vector so we reshape the
+    # resulting array such that we preserve the shape of the original.
+    # This is because NumPy functions get rid of the axis that is equal to 1 in
+    # the output
+    elif (len(array.shape) == 2) and (1 in array.shape):
+        cumsum = np.cumsum(array)
+        cumsum = cumsum.reshape(array.shape)
+    # If it is a two-dimensional array with no axis with size 1, we do the
+    # cumulative sum over its columns
     elif (len(array.shape) == 2) and ((array.shape[0] != 1) and (array.shape[1] != 1)):
         cumsum = np.cumsum(array, axis=0)
     # If it's a matrix with n axis, where n > 2, we do the cumulative sum
@@ -103,12 +123,15 @@ def cumprod(array: np.ndarray):
 
     # We check that the parameter is of the right type
     if not isinstance(array, np.ndarray):
-        raise TypeError('array variable is not of type ndarray.')
+        raise TypeError('array parameter is not of type ndarray.')
     
     # If it's a vector, we merely calculate the cumulative product over all of
     # its elements
-    if (len(array.shape) == 1) or ((len(array.shape) == 2) and ((array.shape[0] == 1) or (array.shape[1] == 1))):
+    if len(array.shape) == 1:
         cumprod = np.cumprod(array)
+    elif (len(array.shape) == 2) and (1 in array.shape):
+        cumprod = np.cumprod(array)
+        cumprod = cumprod.reshape(array.shape)
     # If it's a matrix, we calculate the cumulative product over its columns
     elif (len(array.shape) == 2) and ((array.shape[0] != 1) and (array.shape[1] != 1)):
         cumprod = np.cumprod(array, axis=0)
@@ -144,26 +167,30 @@ def diff(array: np.ndarray):
 
     # We check that the parameter is of the right type
     if not isinstance(array, np.ndarray):
-        raise TypeError('array variable is not of type ndarray.')
+        raise TypeError('array parameter is not of type ndarray.')
     
-    # If it's a row vector, we calculate the difference over all of
-    # its elements. A vector may have one or two dimensions in NumPy; row
+    # If it's an array with one dimension, we calculate the difference over all
+    # of its elements. A vector may have one or two dimensions in NumPy; row
     # vectors have one element in their shape
     if len(array.shape) == 1:
         diff = np.diff(array)
-    # However column vectors have two; also, we need to specify the right
-    # axis when having a column vector to get a proper vector of
-    # differences
+    # If we have an array with two dimensions, we need to specify the right
+    # axis when having a column vector to get a proper vector of differences
     elif len(array.shape) == 2:
         if array.shape[0] == 1:
             diff = np.diff(array)
         elif array.shape[1] == 1:
             diff = np.diff(array, axis=0)
-    # If it's a matrix, we calculate the difference between its rows
+        if (len(array.shape) == 2) and (1 in array.shape):
+            # If we're dealing with a row or column vector, we reshape the
+            # resulting array such that we preserve the shape of the original
+            diff = diff.reshape(array.shape)
+    # If it's an array with two dimensions and one of those has size different
+    # than zero, we calculate the difference between its rows
     elif (len(array.shape) == 2) and (array.shape[0] != 1) and (array.shape[1] != 1):
         diff = np.diff(array, axis=0)
-    # If it's an array with n > 2 axis, we do the difference over
-    # the first axis we find that has more than one element
+    # If it's an array with n > 2 axis, we do the difference over the first
+    # axis we find that has more than one element
     elif len(array.shape) > 2:
         for idx, i in enumerate(array.shape):
             if i > 1:
@@ -194,16 +221,22 @@ def fliplr(array: np.ndarray):
 
     # We check that the parameter is of the right type
     if not isinstance(array, np.ndarray):
-        raise TypeError('array variable is not of type ndarray.')
+        raise TypeError('array parameter is not of type ndarray.')
 
-    # If we have a row vector, we flip left to right normally, with flip()
-    if (len(array.shape) == 1) or ((len(array.shape) == 2) and ((array.shape[0] == 1) or (array.shape[1] == 1))):
+    # If we have a one-dimensional array, we flip left to right with np.flip()
+    if (len(array.shape) == 1):
         fliplr = np.flip(array)
-
-    # If we have a column vector, we flip left to right normally, with fliplr()
-    elif (len(array.shape) == 2) and ((array.shape[0] != 1) and (array.shape[1] != 1)):
-        fliplr = np.fliplr(array)
-
+    # If we have a two-dimensional array with the first axis having size 1,
+    # that could be considered the equivalent of a MATLAB row vector, so
+    # we flip as before
+    elif (len(array.shape) == 2):
+        if array.shape[0] == 1:
+            fliplr = np.flip(array)
+        # If we have a column vector, we flip left to right with np.fliplr()
+        if array.shape[1] == 1:
+            fliplr = np.fliplr(array)
+        # Preserving the shape of the input
+        fliplr = fliplr.reshape(array.shape)
     # Proper implementation of multidimensional array flipping not complete
     elif len(array.shape) > 2:
         for idx, i in enumerate(array.shape):
@@ -235,12 +268,20 @@ def mean(array: np.ndarray):
 
     # We check that the parameter is of the right type
     if not isinstance(array, np.ndarray):
-        raise TypeError('array variable is not of type ndarray.')
+        raise TypeError('array parameter is not of type ndarray.')
     
-    # If it's a vector, we merely calculate the mean over all of its elements
-    if (len(array.shape) == 1) or ((len(array.shape) == 2) and ((array.shape[0] == 1) or (array.shape[1] == 1))):
+    # If it's a one-dimensional array, we merely calculate the mean over all of
+    # its elements
+    if len(array.shape) == 1:
         mean = np.mean(array)
-    # If it's a two dimensional array, we calculate the mean over the columns.
+    # If it's a two-dimensional array with size of any axis equal to 1, we're
+    # dealing with the equivalent of a row or column vector, so we reshape the
+    # resulting array such that we preserve the shape of the original
+    if (len(array.shape) == 2) and ((array.shape[0] == 1) or (array.shape[1] == 1)):
+        mean = np.mean(array)
+        mean = mean.reshape(array.shape)
+    # If it's a two dimensional array with no axis with size equal to 1, we
+    # calculate the mean over the columns
     elif (len(array.shape) == 2) and ((array.shape[0] != 1) and (array.shape[1] != 1)):
         mean = np.mean(array, axis=1)
     # If it's a matrix with more than n axis, we calculate the mean over
@@ -275,12 +316,19 @@ def sum(array: np.ndarray):
 
     # We check that the parameter is of the right type
     if not isinstance(array, np.ndarray):
-        raise TypeError('array variable is not of type ndarray.')
+        raise TypeError('array parameter is not of type ndarray.')
     
-    # If it's a vector, we merely calculate the sum over all of its elements
-    if (len(array.shape) == 1) or ((len(array.shape) == 2) and ((array.shape[0] == 1) or (array.shape[1] == 1))):
+    # If it's a one-dimensional, we merely calculate the sum over all of its elements
+    if len(array.shape) == 1:
         sum = np.sum(array)
-    # If it's a matrix, we calculate the mean over its columns
+    # If it's a two-dimensional array with size of any axis equal to 1, we're
+    # dealing with the equivalent of a row or column vector, so we reshape the
+    # resulting array such that we preserve the shape of the original
+    elif (len(array.shape) == 2) and ((array.shape[0] == 1) or (array.shape[1] == 1)):
+        sum = np.sum(array)
+        sum = sum.reshape(array.shape)
+    # If it's a two-dimensional array with no size of any axis equal to 1, we
+    # calculate the mean over its columns
     elif (len(array.shape) == 2) and ((array.shape[0] != 1) and (array.shape[1] != 1)):
         sum = np.sum(array, axis=1)
     # If it's an array with n axis where n > 2, we calculate the mean
